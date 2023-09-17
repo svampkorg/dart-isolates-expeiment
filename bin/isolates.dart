8d67c6418eb3b190a475ba5d2d81075911501e3c
Bo Martin Nilsson <bo-martin.nilsson@consid.se>
2023-09-17 16:18:00 +0200

    formatting

--- bin/isolates.dart
+++ bin/isolates.dart
@@ -22,21 +22,21 @@ void main() async {
 
   var bcFromIsolates = workerReceivePort.asBroadcastStream();
 
-fibbFutures = [
-  IsolateRunner.run<int, int>(fibonacci, fibb1, null, workerReceivePort.sendPort,
-      isolateName: "fibonacci calculation for $fibb1:th number"),
-  IsolateRunner.run<int, int>(fibonacci, fibb2, null, workerReceivePort.sendPort,
-      isolateName: "fibonacci calculation for $fibb2:th number"),
-  IsolateRunner.run<int, int>(fibonacci, fibb3, null, workerReceivePort.sendPort,
-      isolateName: "fibonacci calculation for $fibb3:th number"),
-  IsolateRunner.run<int, int>(fibonacci, fibb4, null, workerReceivePort.sendPort,
-      isolateName: "fibonacci calculation for $fibb4:th number"),
-  IsolateRunner.run<int, int>(fibonacci, fibb5, null, workerReceivePort.sendPort,
-      isolateName: "fibonacci calculation for $fibb5:th number"),
-  // IsolateRunner.run<int, int>(fibonacci, fibb6, null, workerReceivePort.sendPort,
-  //     isolateName: "fibonacci calculation for $fibb6:th number"),
-  // IsolateRunner.run<int, int>(fibonacci, fibb7, null, workerReceivePort.sendPort,
-  //     isolateName: "fibonacci calculation for $fibb7:th number"),
+  fibbFutures = [
+    IsolateRunner.run<int, int>(fibonacci, fibb1, null, workerReceivePort.sendPort,
+        isolateName: "fibonacci calculation for $fibb1:th number"),
+    IsolateRunner.run<int, int>(fibonacci, fibb2, null, workerReceivePort.sendPort,
+        isolateName: "fibonacci calculation for $fibb2:th number"),
+    IsolateRunner.run<int, int>(fibonacci, fibb3, null, workerReceivePort.sendPort,
+        isolateName: "fibonacci calculation for $fibb3:th number"),
+    IsolateRunner.run<int, int>(fibonacci, fibb4, null, workerReceivePort.sendPort,
+        isolateName: "fibonacci calculation for $fibb4:th number"),
+    IsolateRunner.run<int, int>(fibonacci, fibb5, null, workerReceivePort.sendPort,
+        isolateName: "fibonacci calculation for $fibb5:th number"),
+    // IsolateRunner.run<int, int>(fibonacci, fibb6, null, workerReceivePort.sendPort,
+    //     isolateName: "fibonacci calculation for $fibb6:th number"),
+    // IsolateRunner.run<int, int>(fibonacci, fibb7, null, workerReceivePort.sendPort,
+    //     isolateName: "fibonacci calculation for $fibb7:th number"),
   ];
 
   workerReceivePort.listenFor<Map<String, int>>(
