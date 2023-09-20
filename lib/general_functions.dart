4a332b6059876571e27fca9a09bcc6ef245cc42b
Bo Martin Nilsson <bo-martin.nilsson@consid.se>
2023-09-20 09:15:18 +0200

    changed fib to 1 line

--- lib/general_functions.dart
+++ lib/general_functions.dart
@@ -36,9 +36,4 @@ Completer<T> wrapInCompleter<T>(Future<T> future) {
   return completer;
 }
 
-int fibonacci(int n) {
-  if (n == 0 || n == 1) {
-    return n;
-  }
-  return fibonacci(n - 1) + fibonacci(n - 2);
-}
+int fibonacci(int n) => n < 2 ? n : fibonacci(n - 1) + fibonacci(n - 2);
